48ad415e6f60725b660e95a2416ee58e8188d820
Larissa S <larissafschiavo@gmail.com>
2017-10-31 02:00:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,5 +52,15 @@ class TicTacToe
       display_board
   end
 
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
+
+  
+
 
 end
